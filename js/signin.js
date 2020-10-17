4c0e871c804b009d9bfd3c05f09f552026560caa
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-10-17 17:01:35 +0700

    n

--- js/signin.js
+++ js/signin.js
@@ -30,7 +30,19 @@ $(function () {
 
     });
 
+    const auth = firebase.auth();
 	
+	
+	function signUp(){
+		
+		var email = document.getElementById("email");
+		var password = document.getElementById("password");
+		
+		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+		promise.catch(e => alert(e.message));
+		
+		alert("Signed Up");
+	}
 	
    
     
