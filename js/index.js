bc5f9bf41d84adbfab2fb05dedbef4559ebff053
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-10-17 22:28:32 +0700

    n

--- js/index.js
+++ js/index.js
@@ -1,38 +1,19 @@
-// $(function () {
-
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-//             // User is signed in.
-//             displayName = user.displayName;
-//             email = user.email;
-//             photoUrl = user.photoURL;
-//             console.log(displayName, email, photoUrl);
-
-//             $("#username").text(email);
-//             $("#displayname").text(displayName);
-//             $("#photo").attr("src",photoUrl);
-
-//         } else {
-//             window.location.href = 'signin.html';
-//         }
-//     });
-
-//     var db = firebase.firestore();
-//     db.collection("movie")
-//   .get()
-//   .then((querySnapshot) => {
-//       querySnapshot.forEach((doc)=>{
-//           var card =
-//         `<div class="card">
-//           <img class="card-img-top" src="${doc.data().posterURL}" alt="">
-//           <div class="card-doby">
-//              <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
-//              <p class="card-text">${doc.data().detail}</p>
-//              </div>
-//         </div>`
-//         $("#list").append(card);
-//         $("#list2").append(card);
-//       }
-//       )
-//   }
-//   )});
+    var db = firebase.firestore();
+    db.collection("movie")
+  .get()
+  .then((querySnapshot) => {
+      querySnapshot.forEach((doc)=>{
+          var card =
+        `<div class="card">
+          <img class="card-img-top" src="${doc.data().posterURL}" alt="">
+          <div class="card-doby">
+             <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
+             <p class="card-text">${doc.data().detail}</p>
+             </div>
+        </div>`
+        $("#list").append(card);
+        $("#list2").append(card);
+      }
+      )
+  }
+  );
--- js/signin.js
+++ js/signin.js
@@ -33,69 +33,68 @@ function signOut(){
   window.location.href = 'signin.html'
 }
 
-
 $(function () {
-var db = firebase.firestore();
-    db.collection("movie")
-  .get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc)=>{
-          var card =
-        `<div class="card">
-          <img class="card-img-top" src="${doc.data().posterURL}" alt="">
-          <div class="card-doby">
-             <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
-             <p class="card-text">${doc.data().detail}</p>
-             </div>
-        </div>`
-        $("#list").append(card);
-        $("#list2").append(card);
+
+  firebase.auth().onAuthStateChanged(function (email) {
+      if (email) {
+          // User is signed in.
+          displayName = email.displayName;
+          email = email.email;
+          photoUrl = email.photoURL;
+          console.log(displayName, email, photoUrl);
+
+          $("#username").text(email);
+          $("#displayname").text(displayName);
+          $("#photo").attr("src",photoUrl);
+
+      } else {
+          
       }
-      )
-  }
-  )});
+  })});
 
-    // $("#signingoogle").click(function(){
+
+
+
+// $(function () {
+// var db = firebase.firestore();
+//     db.collection("movie")
+//   .get()
+//   .then((querySnapshot) => {
+//       querySnapshot.forEach((doc)=>{
+//           var card =
+//         `<div class="card">
+//           <img class="card-img-top" src="${doc.data().posterURL}" alt="">
+//           <div class="card-doby">
+//              <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
+//              <p class="card-text">${doc.data().detail}</p>
+//              </div>
+//         </div>`
+//         $("#list").append(card);
+//         $("#list2").append(card);
+//       }
+//       )
+//   }
+//   )});
+
+
+    $("#signingoogle").click(function(){
     
-    //     var provider = new firebase.auth.GoogleAuthProvider();
-    //     provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    //     firebase.auth().signInWithRedirect(provider);
-    //     firebase.auth().getRedirectResult().then(function(result) {
-    //         if (result.credential) {
-    //           // This gives you a Google Access Token. You can use it to access the Google API.
-    //           var token = result.credential.accessToken;
-    //         }
-    //         // The signed-in user info.
-    //         var user = result.user;
-    //       }).catch(function(error) {
-    //         // Handle Errors here.
-    //         var errorCode = error.code;
-    //         var errorMessage = error.message;
-    //         console.log(errorCode);
-    //         $("#error").text(errorMessage);
-    //       });
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function(result) {
+            if (result.credential) {
+              // This gives you a Google Access Token. You can use it to access the Google API.
+              var token = result.credential.accessToken;
+            }
+            // The signed-in user info.
+            var user = result.user;
+          }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            $("#error").text(errorMessage);
+          });
 
-    // });
-
-
-
-
-        
-    // $("#signinemail").click(function(){
-
-    //     var email = $("#email").val();
-    //     var password = $("#password").val();
-
-    //     firebase.auth()
-    //     .signInWithEmailAndPassword(email, password)
-    //     .catch(function(error) {
-    //         // Handle Errors here.
-    //         var errorCode = error.code;
-    //         var errorMessage = error.message;
-    //         console.log(errorCode);
-    //         $("#error").text(errorMessage);
-    //         // ...
-    //       });
-    // });
-
-// })
+    });
