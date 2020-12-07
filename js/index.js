0ac9fa9da564088ad52bf28ec229dbe973f901e0
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-12-07 18:09:07 +0700

    n

--- js/index.js
+++ js/index.js
@@ -1,20 +1,62 @@
-
- var db = firebase.firestore();
-    db.collection("movie")
-  .get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc)=>{
-        var c = `${doc.data().listN}`
-          var card =
-        `<div class="card">
+function m() {
+  var db = firebase.firestore();
+  db.collection("movie")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var num = `${doc.data().listN}`
+        var card =
+          `<div class="card">
           <img class="card-img-top" src="${doc.data().posterURL}" alt="">
           <div class="card-doby">
              <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
              <p class="card-text">${doc.data().detail}</p>
              </div>
-        </div>`
-        $("#list2"+c).append(card);
+        </div>
+        <center>
+        <div class="bass">Trailer</div>
+                    <iframe src="${doc.data().Trailer}" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+                    <center>`
+          $("#D"+ num).append(card);
+        
       }
       )
+    }
+    )
+};
+
+$(function () {
+  var db = firebase.firestore();
+  db.collection("movie").get().then((querySnapshot) => {
+         
+      querySnapshot.forEach((doc) => {
+              var num = `${doc.data().listN}`
+              var card = ` <img src="${doc.data().posterURL}" width="375" height="550" id="m1">
+              `;
+              $("#m" + num).append(card);
+          });
+      })
+})
+
+
+var db = firebase.firestore();
+db.collection("movie")
+.get()
+.then((querySnapshot) => {
+  querySnapshot.forEach((doc)=>{
+    var c = `${doc.data().listN}`
+      var card =
+    `<div class="card">
+      <img class="card-img-top" src="${doc.data().posterURL}" alt="">
+      <div class="card-doby">
+         <h4 class="card-title">${doc.data().title} - ${doc.data().year}</h4>
+         <p class="card-text">${doc.data().detail}</p>
+         </div>
+    </div>`
+    $("#list2"+c).append(card);
   }
-  );
+  )
+}
+);
